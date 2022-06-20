dd0d0256639108fb1098f226a6d6975234382400
Mohamad Al khatib <mohamad@alkhatib.com>
2022-06-20 13:37:50 +0200

    first commit

--- controller/main.js
+++ controller/main.js
@@ -1,7 +1,3 @@
 exports.main = (req, res) => {
-	const multiplier = ({ a, b }) => a * b;
-
-	const result = multiplier({ a: 2, b: 3 });
-
-	res.render("../view/main.ejs", { result });
+	res.render("../view/main.ejs", {});
 };
